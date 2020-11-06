f54deeafe926b26be867b44d623b33d0194e2df2
vicnhyi_myzuka <yaroslav.valentyi@ed-era.com>
2020-11-06 12:27:34 +0200

    Added 404 + webpack chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Auth from '../views/Auth.vue'
-import Home from '../views/Home.vue'
 import store from '../store/index'
 Vue.use(VueRouter)
 const routes = [
@@ -9,21 +7,18 @@ const routes = [
     path: '/',
     alias: '/home',
     name: 'Home',
-    component: Home,
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
     meta: { requiredAuth: true }
   },
   {
     path: '/auth',
     name: 'Authentication',
-    component: Auth
+    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    name: '404',
+    component: () => import(/* webpackChunkName: "auth" */ '../views/404.vue')
   }
 ]
 
